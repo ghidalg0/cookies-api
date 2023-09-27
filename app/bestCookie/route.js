aafb29ef69be7af033862f0369913a84f4963b55
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2023-09-27 08:44:58 +0200

    test for 2s timeout

--- app/bestCookie/route.js
+++ app/bestCookie/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET() {
   const cookie = {
     name: "Chocolate Cookie",
@@ -6,10 +8,9 @@ export async function GET() {
     picture: "choco",
   };
 
-  return new Response(JSON.stringify({ cookie }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  // Add a delay of 2 seconds before sending the response
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // Use NextResponse to set the JSON response and status code
+  return NextResponse.json({ cookie }).setStatus(200);
 }
